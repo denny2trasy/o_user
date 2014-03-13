65bc386d50b6cd9749b0e84919ef73b43a024aed
denny2trasy <denny2trasy@gmail.com>
2014-03-13 23:46:18 +0800

    add secret token

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ElUser::Application.config.secret_token = 'e75165b4c60ec5e5c247c6c406488e68556ea31e50d3d3a5156fb6c9c85a40e4e9e0d29786989e5195741a29c46886605318385dd91605ef69524251020ae952'
