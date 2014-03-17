f2571f4b03e088a6e26939b0a31ebdbb6aa9cea8
denny2trasy <denny2trasy@gmail.com>
2014-03-17 11:40:59 +0800

    change new secret token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,5 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-ElUser::Application.config.secret_token = 'e75165b4c60ec5e5c247c6c406488e68556ea31e50d3d3a5156fb6c9c85a40e4e9e0d29786989e5195741a29c46886605318385dd91605ef69524251020ae952'
+# ElUser::Application.config.secret_token = 'e75165b4c60ec5e5c247c6c406488e68556ea31e50d3d3a5156fb6c9c85a40e4e9e0d29786989e5195741a29c46886605318385dd91605ef69524251020ae952'
+ElUser::Application.config.secret_token = 'a9f335bb8e278818b7df1e595df4b529ce309003c7f6d823ede2a6b050562c1c457e12b1462a583294de5b846192e30ece1e404788ea47db0b3f08b560f61c64'
